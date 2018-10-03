d9e1da954d7ba39165d0fa995d1187839e3e6ad3
RED <red@sharemind.kr>
2018-10-04 02:54:40 +0900

    181004 -인덱스 목록 수정(위치표시, 줄바꿈) -북마크기능 수정 -플레이어정보 수정 -버전업 1.0.3

--- assets/learnplayer/js/app.js
+++ assets/learnplayer/js/app.js
@@ -97,7 +97,7 @@ window.defaultOpts = {
         enableFullscreen: true,
         enableNumbers: true,
         enableJogStyle: false,
-        alwaysCaptureHotkeys: true,
+        alwaysCaptureHotkeys: false,
         enableModifiersForNumbers: true,
         enableInactiveFocus: true,
         skipInitialFocus: false,
@@ -128,15 +128,66 @@ window.defaultOpts = {
             title: '' // default '알림'
         }
     },
+    bookmarks: {
+        items:[],
+        isUse: true,
+        extraFunctions:{
+            add: addBookmark,
+            modify: modifyBookmark,
+            remove: deleteBookmark
+        }
+    },
     playerInfo: {
-        innerHtml: 'EOSF player 1.0' // text or tag Element
+        innerHtml: 'EBS sharePlayer' // text or tag Element
     },
     debug: true
 };
 
+/** ****************
+ * ## README ##
+ * defaultOpts이나 플레이어 초기화 옵션에 지정하여 사용할 함수정의 입니다
+ * 현파일에 구현하여 글로벌 공통함수로 사용하거나 특정 페이지에 별도 또는 재정의 하여 사용하여도 됩니다.
+ * 비사용시 삭제하여도 무방합니다.
+ */
+
+ /**
+ * 옵션 bookmarks.extraFunctions.add
+ * 플레이어에 추가된 북마크 아이템의 정보를 전달합니다.
+ * @param {Object} [item]
+ * @param {Component~ReadyCallback} callbackFunction
+ *  서버로 raw 데이터 전송후 가공된 데이터를 플레이어에 전달할 목적으로 사용됩니다.
+ */
+function addBookmark() {
+    var item = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    var callbackFunction = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+    item.dbSeq = parseInt( Math.random() * 10000 );
+    callbackFunction(item);
+}
+
 /**
- * topbar > qnaButton callback function
- * href 경로 지정을 무효하고 함수에서 기능을 구현합니다.
+ * 옵션 bookmarks.extraFunctions.modify
+ * 플레이어에 수정된 북마크 아이템의 정보를 전달합니다.
+ * @param {Object} [item]
+ * @param {Component~ReadyCallback} callbackFunction
+ *  서버로 raw 데이터 전송후 가공된 데이터를 플레이어에 전달할 목적으로 사용됩니다.
+ */
+function modifyBookmark() {
+    var item = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    var callbackFunction = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+}
+
+/**
+ * 옵션 bookmarks.extraFunctions.modify
+ * 플레이어에 삭제된 북마크 아이템의 정보를 전달합니다.
+ * @param {Object} [item]
+ */
+function deleteBookmark() {
+    var item = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+}
+
+/**
+ * 옵션 topbar.qnaButton.extraFunction
+ * topbar.qnaButton.href 경로 지정을 무효하고 함수에서 기능을 구현합니다.
  */
 function goQNApopup() {
     alert('QNA 팝업을 직접 처리합니다.');
